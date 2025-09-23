9d1423111deef94a6ee21a79187d792d42dea9da
s19013 <hideya670@gmail.com>
2025-09-23 22:27:02 +0900

    sanctumの下準備として､laravelがやれといてるからやる

---
+++ laravel/app/Models/Sanctum/PersonalAccessToken.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models\Sanctum;
+
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
+
+// ぶっちゃけ私もよくわかってないけど､laravel公式がやれって言ってるからやる
+class PersonalAccessToken extends SanctumPersonalAccessToken
+{
+    // ...
+}
--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ぶっちゃけ私もよくわかってないけど､laravel公式がやれって言ってるからやる
+
+        // Sanctumが提供している、usePersonalAccessTokenModelメソッドを利用し、カスタムモデルを使用するようにSanctumへ指示します
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }
