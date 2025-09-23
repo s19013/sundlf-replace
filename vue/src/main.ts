5ac048c195a8fba9ae9519ccbf72cfcfd0654462
s19013 <hideya670@gmail.com>
2025-09-24 00:25:43 +0900

    cors設定と,axiosを使えるようにする準備

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axiosPlugin  from './plugin/axios'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(axiosPlugin, 'env');
 
 app.mount('#app')
---
+++ vue/src/plugin/axios.ts
@@ -0,0 +1,30 @@
+import type { App } from 'vue'
+import axios from 'axios';
+
+const option = {
+  // cors設定
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+    // 'UserLang': window.navigator.language,
+  },
+  baseURL:'',
+}
+
+const axiosPlugin = {
+  install(app: App, env: string) {
+    // 開発
+    if (env === "local" || env === 'dev') {
+      option.baseURL = 'http://127.0.0.1:8000/api/';
+    }
+
+    // 本番
+    if (env === "product" || env === 'pro') {
+      option.baseURL = 'https://sundlf.com/api/';
+    }
+
+    const axiosInstance = axios.create(option);
+    app.provide('axios', axiosInstance);
+  },
+};
+export default axiosPlugin;
