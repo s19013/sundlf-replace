df4cacc1870fe74769e08b343ef3ff8ecf780a69
s19013 <hideya670@gmail.com>
2025-09-23 22:35:04 +0900

    sanctumで使うミドルウェアを追加

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // sanctumで必要
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
