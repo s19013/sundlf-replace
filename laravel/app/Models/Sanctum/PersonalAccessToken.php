d5e1dd7ed131848e85bc18964e53b15e50b4f689
s19013 <hideya670@gmail.com>
2025-09-23 22:34:33 +0900

    コメント編集

--- laravel/app/Models/Sanctum/PersonalAccessToken.php
+++ laravel/app/Models/Sanctum/PersonalAccessToken.php
@@ -3,7 +3,7 @@ namespace App\Models\Sanctum;
 
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-// ぶっちゃけ私もよくわかってないけど､laravel公式がやれって言ってるからやる
+// ぶっちゃけ私もよくわかってないけど､laravel公式がやれって言ってるから生成する
 // sanctumで必要
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
