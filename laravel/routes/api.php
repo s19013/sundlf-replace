866e919c48909bf690325cfd8f4517deb82fa072
s19013 <hideya670@gmail.com>
2025-09-24 20:40:39 +0900

    testapiを渡す

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function (Request $request) {
+    return response()->json([
+        'message' => 'api test success',
+    ]);
+});
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
