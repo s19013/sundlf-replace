f667a272fd26c5e5c66c90dcda4652fcd3e68f4a
s19013 <hideya670@gmail.com>
2025-09-24 21:38:05 +0900

    format適応

--- vue/src/plugin/axios.ts
+++ vue/src/plugin/axios.ts
@@ -1,17 +1,16 @@
-import type { App,InjectionKey  } from 'vue'
+import type { App, InjectionKey } from 'vue'
 import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 
 export const axiosKey: InjectionKey<AxiosInstance> = Symbol('axios')
-
 
 const option = {
   // cors設定
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
-    'xsrfCookieName':'XSRF-TOKEN',
-    'xsrfHeaderName':'X-XSRF-TOKEN'
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: 'X-XSRF-TOKEN',
     // 'UserLang': window.navigator.language,
   },
   baseURL: 'http://localhost:8000/api/',
