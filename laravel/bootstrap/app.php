3c5a452bc8ff03bd8902816775dfc3885b214e91
s19013 <hideya670@gmail.com>
2025-10-13 01:27:10 +0900

    spa認証を辞めるからこれはいらない

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -12,8 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // sanctumで必要
-        $middleware->statefulApi();
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
