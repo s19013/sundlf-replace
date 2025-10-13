e90cca123c488d21c4702ea1d64dd86be0162d4a
s19013 <hideya670@gmail.com>
2025-10-13 22:43:46 +0900

    ログイン済みユーザーしかアクセスできないapiと通信テストする

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { inject, onMounted } from 'vue'
+import { axiosKey } from '@/plugin/axios'
+import type { AxiosInstance, AxiosResponse, AxiosError } from 'axios'
+
+// このプロジェクトようのaxiosを呼び出す
+const axios = inject<AxiosInstance>(axiosKey)
+
+onMounted(() => {
+  if (!axios) return
+  axios
+    .get('user')
+    .then((response: AxiosResponse) => {
+      console.log(response)
+    })
+    .catch((error: AxiosError) => {
+      console.error(error.response?.data ?? error.message)
+    })
+})
 </script>
 
 <template>
