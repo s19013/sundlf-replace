859698edd99d24498b63b62edff314e925a896e1
s19013 <hideya670@gmail.com>
2025-10-25 17:56:59 +0900

    routeとrouterを分けた｡ それ用の型があるからそれを使うように修正

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,24 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import { AuthGuard } from '@/router/authGuard'
 import { GuestGuard } from '@/router/guestGuard'
 
 const authGuard = new AuthGuard()
 const guestGuard = new GuestGuard()
 
+const routes: RouteRecordRaw[] = [
+  ...authGuard.accessibleList,
+  ...guestGuard.accessibleList,
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    ...authGuard.accessibleList,
-    ...guestGuard.accessibleList,
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
 })
 
 router.beforeEach(authGuard.routeGuard)
